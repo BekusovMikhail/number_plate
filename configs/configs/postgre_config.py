14c78595ba20f55932320a52816498a22b06cc01
BekusovMihail <mbekusov@bk.ru>
2023-10-29 20:11:25 +0300

    Rm redundant files

--- configs/configs/postgre_config.py
+++
@@ -1,4 +0,0 @@
-posgres_DB = "plate_detection_db"
-posgres_user = "plate_detection_user"
-posgres_user_password = "lprpassword"
-sql_alchemy_engine = f"postgresql+psycopg2://{posgres_user}:{posgres_user_password}@127.0.0.1/{posgres_DB}"
